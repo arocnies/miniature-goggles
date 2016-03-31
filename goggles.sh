d653c722619d5b5a2d55bdedec2da8404f656adc
anies <arocnies@gmail.com>
2016-03-31 13:42:01 -0400

    Many fixes. eval '&' moved inside the statement so as to bg the command rather than the eval itself.

--- goggles.sh
+++ goggles.sh
@@ -18,17 +18,26 @@
 
 # Run the command and save the PID to a variable.
 COMMAND=$1
-eval ${COMMAND} &
+CMD_PID=''
+eval "(${COMMAND}) &"
 CMD_PID=$!
 
 # If this script is killed, kill the command.
 trap "kill ${CMD_PID} 2> /dev/null" EXIT
 
 # Output PID and command
-top -p ${CMD_PID} -n 1 -b | tail -fn 1 | awk '{printf "%s,%s\n",$1,$12}'
+#top -p ${CMD_PID} -n 1 -b | tail -fn 1 | awk '{printf "%s,%s\n",$1,$12}'
+echo $CMD_PID,\"$COMMAND\"
 
 # Monitor loop while proc is running.
+COUNT=0
 while kill -0 ${CMD_PID} 2> /dev/null; do
-top -p ${CMD_PID} -n 1 -b | tail -fn 1 | awk '{printf "%s,%s,%s\n",$11,$10,$9}'
+    #top -p ${CMD_PID} -n 1 -b | tail -fn 1 | awk '{printf "%s,%s,%s\n",$11,$10,$9}'
+    if [[ $COUNT==0 ]]; then
+        ps -o vsz,etime ${CMD_PID}
+    else
+        ps -o vsz=,etime= ${CMD_PID}
+    fi
+    sleep 1
+    $(( $COUNT++ ))
 done
-
