b3364d363fe1e5428832875e4edba4bc943224ac
anies <arocnies@gmail.com>
2016-04-11 15:12:24 -0400

    BuildInstall script now makes man page. Man page changes.

--- buildInstall.sh
+++ buildInstall.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+version="0.5"
 
+# Build man page
+ronn goggles.1.ronn
+mv goggles.1 miniature-goggles_$version/usr/local/share/man/man1/
+
+# Build and install .deb
 dpkg-deb --build miniature-goggles_0.5
 dpkg -i miniature-goggles_0.5.deb
